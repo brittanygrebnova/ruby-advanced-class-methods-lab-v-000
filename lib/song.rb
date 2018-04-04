ba52681ee33b7fa9e9cb4e3237233569c9d7adc2
Brittany Grebnov <brittanygrebnova@gmail.com>
2018-04-04 03:20:38 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,47 @@ class Song
   def save
     self.class.all << self
   end
-
+  
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+  
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+  
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    song
+  end
+  
+  def self.find_by_name(name)
+    @@all.detect{|song| song.name == name}
+  end
+  
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+  
+  def self.alphabetical
+    @@all.sort_by { |song| song.name }
+  end
+  
+  def self.new_from_filename(song)
+    separated = song.split(/[-.]/)
+    separated.each do |part|
+      separated.pop
+      name = separated[0]
+      artist_name = separated[1]
+      song = self.new
+      song.name = name
+      song.artist_name = artist_name
+    end
+  end
 end
